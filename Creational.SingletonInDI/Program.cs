e98a587b198889aead0fb09ea0d9b9c147294a32
dodinhnguyen <nguyendo891@yahoo.com>
2019-11-29 16:27:03 +0100

    Add project files.

---
+++ Creational.SingletonInDI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.SingletonInDI
+{
+    public class Foo
+    {
+        public EventBroker Broker;
+
+        public Foo(EventBroker broker)
+        {
+            Broker = broker ?? throw new ArgumentNullException(paramName: nameof(broker));
+        }
+    }
+
+    public class EventBroker
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<EventBroker>().SingleInstance();
+        builder.RegisterType<Foo>();
+
+        using (var c = builder.Build())
+        {
+            var foo1 = c.Resolve<Foo>();
+            var foo2 = c.Resolve<Foo>();
+
+            Console.WriteLine(ReferenceEquals(foo1, foo2));
+            Console.WriteLine(ReferenceEquals(foo1.Broker, foo2.Broker));
+        }
+    }
+    }
+}
